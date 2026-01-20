a3d1879a038abb233cd769a98b175ee8700be9ec
ale <spartano.alessio07@gmail.com>
2026-01-20 12:55:46 +0100

    Creazione branch client

--- server/gpoProject_Server/src/main/java/it/edu/maxplanck/gpoProject_Server/dto/request/TestUserDTO.java
+++
@@ -1,8 +0,0 @@
-package it.edu.maxplanck.gpoProject_Server.dto.request;
-
-public record TestUserDTO(
-	String username,
-	String password,
-	Boolean isAdmin,
-	String imagePath
-) {}
--- server/gpoProject_Server/src/main/java/it/edu/maxplanck/gpoProject_Server/util/UtilServer.java
+++
@@ -1,7 +0,0 @@
-package it.edu.maxplanck.gpoProject_Server.util;
-
-public final class UtilServer {
-	
-	public static final int timeExpirationDateCookieToken = 60 * 60;
-	
-}
