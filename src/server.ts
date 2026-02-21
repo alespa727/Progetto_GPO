ad5c686d0e6a69ad83d739fde37d8d0032b97571
ale <spartano.alessio07@gmail.com>
2026-02-21 21:44:45 +0100

    Autenticazione tramite jwt e messaggi in tempo reale gestiti molto meglio con dati della chat aggiornati dal server

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,13 @@
-import {
-    Channel,
-    TextChannel,
-    Server,
-    Section,
-    Message,
-    ChannelType,
-    VoiceChannel,
-    endpoint
-} from "./types";
- 
+
 import 'dotenv/config';
 import express from "express";
 import { createServer } from "http";
-import { Socket, Server as SocketServer } from "socket.io";
+import { Server as SocketServer } from "socket.io";
 import cors from "cors";
-import { send } from "process";
 import { AccessToken } from 'livekit-server-sdk';
-import { randomInt } from "crypto";
 import axios from "axios";
-// proxy.js
-import { createProxyMiddleware } from "http-proxy-middleware";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { use } from 'react';
 
 const apiKey = process.env.LIVEKIT_API_KEY;
 const apiSecret = process.env.LIVEKIT_API_SECRET;
@@ -29,79 +17,180 @@ app.use(cors());
 app.use(express.json());
 
 
+const keyBase64 = "Ink1VTlacUY0djlVLzVkRXFLc2pRWGZTZ1QwUUg4elh3WDdPZm1BNE1acTg9Ig==";
+const secret = Buffer.from(keyBase64, "base64");
+
+export function verifyAccessToken(token: string) {
+    try {
+        const decoded = jwt.verify(token, secret, {
+            algorithms: ["HS256"]
+        })
+        if (typeof decoded === "object" && decoded !== null && "username" in decoded) {
+            return { username: (decoded as JwtPayload).username as string };
+        }
+    } catch (err) {
+        console.error("Access token non valido:", err);
+        return null;
+    }
+}
+
+let cookies: any[] | null | undefined = null;
+
+axios.post("http://localhost:8080/api/login", { username: "ale", password: "727" },
+    {
+        withCredentials: true,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then((response) => {
+        cookies = response.headers["set-cookie"];
+    })
+    .catch((error) => {
+        console.log(error.response?.data);
+    });
+
+
 
 const httpServer = createServer(app);
 const io = new SocketServer(httpServer, {
     cors: { origin: "*" },
 });
 
-let map: Map<string, Socket> = new Map();
-
-function getKeyBySocket(targetSocket: Socket): string | null {
-    for (const [key, socket] of map.entries()) {
-        if (socket === targetSocket) {
-            return key; 
+const getChat = async (id: number) => {
+    const res = await axios.post(
+        "http://localhost:8080/api/admin/chat?chatId=" + id,
+        {},
+        {
+            withCredentials: true,
+            headers: {
+                Cookie: cookies?.join("; ")
+            }
         }
+    )
+
+    const friendship = {
+        user_1: res.data.fkUser1.username,
+        user_2: res.data.fkUser2.username,
     }
-    return null; 
+    return friendship;
 }
 
-let i = 0;
+let deviceCounters: Record<string, number> = {};
 
 io.on("connection", (socket) => {
-    console.log(`Utente connesso: ${socket.id}`);
-    let key: string;
 
-    socket.on("login", (data) => {
-        const { username } = data;
-        key = username
-    });
+    // Codice di autenticazione
+    const token = socket.handshake.auth.token;
 
-    socket.on("disconnecting", () => {
-        socket.rooms.forEach((room: string) => {
-            if (room !== socket.id) {
-                console.log(key + " è uscito da " + room);
-            }
-        });
-    })
+    // Verifica il token di accesso
+    const user = verifyAccessToken(token);
 
+    // Chiusura connessione se token non valido o scaduto
+    if (!user) {
+        console.log("Token non valido, chiudo connessione");
+        socket.disconnect();
+        return;
+    }
+
+    // Salva l'essere online di un determinato utente
+    if (!deviceCounters[user.username]) deviceCounters[user.username] = 0;
+    deviceCounters[user.username] += 1;
+
+    // Salva il numero di dispositivo con cui è online (incrementale)
+    const deviceId = deviceCounters[user.username];
+    const usernameWithDevice = `${user.username}:${deviceId}`;
+
+    // Logs
+    console.log("Utenti online:", deviceCounters);
+    console.log("Nuovo utente autenticato:", usernameWithDevice);
+
+    // Room di socket per comunicazione diretta (notifiche)
+    socket.join(`user_${user.username}`);
+
+    // Gestione disconnessione
     socket.on("disconnect", () => {
-        map.delete(key);
-        console.log(key + " è andato offline");
+        console.log("Utente disconnesso:", usernameWithDevice);
+        console.log("Utenti online:", deviceCounters);
+
+        // Toglie 1 al counter dei dispositivi online
+        deviceCounters[user.username] -= 1;
+        if (deviceCounters[user.username] <= 0) delete deviceCounters[user.username];
     });
 
-    socket.on("join_chat", (data) => {
+    // Entra in una chat tra utenti privati
+    socket.on("join_chat", async (data) => {
         const { chatId } = data;
 
+        const chat = getChat(chatId);
+        console.log(user, "è entrato in", await chat, "con chatId =", chatId);
+
+        // Entra in una room di socket con id della chat
         socket.join(`chat_${chatId}`);
-        console.log(key + " è entrato nella chat_" + chatId);
     });
 
-    socket.on("join_call", (data) => {
+
+    // Entra in una chat tra utenti privati
+    socket.on("leave_chat", async (data) => {
         const { chatId } = data;
 
-        socket.join(`call_${chatId}`);
-        console.log(key + " è entrato nella call_" + chatId);
+        const chat = getChat(chatId);
+        console.log(user, "lascia la stanza", await chat, "con chatId =", chatId);
+
+
+        // Lascia la room di socket con id della chat
+        socket.leave(`chat_${chatId}`);
     });
 
-     socket.on("leave_call", (data) => {
+    // Entra in una chiamata
+    socket.on("join_call", async (data) => {
+        const { chatId } = data;
+        const roomName = `call_${chatId}`;
+
+        // Recupera le informazioni della chat
+        const chat = await getChat(chatId);
+        console.log(user, "vuole entrare in", chat, "con chatId =", chatId);
+
+        // controlla se la room esiste
+        const room = io.sockets.adapter.rooms.get(roomName);
+        const callExists = room && room.size > 0;
+
+        if (callExists) {
+            console.log("La chiamata esiste già, utenti presenti:", room.size);
+        } else {
+            console.log("La chiamata NON esiste, la creo ora");
+            let otherUsername;
+            let caller;
+            if(chat.user_1===user.username){
+                caller = chat.user_1;
+                otherUsername = chat.user_2
+            }else{
+                caller = chat.user_2;
+                otherUsername = chat.user_1
+            }
+            io.to("user_"+otherUsername).emit("incomingCall", { caller });
+        }
+
+        socket.join(roomName);
+    });
+
+    socket.on("leave_call", (data) => {
         const { chatId } = data;
 
         socket.leave(`call_${chatId}`);
-        console.log(key + " è uscito dalla call " + chatId);
     });
 
     socket.on("join_server", (data) => {
         const { serverId } = data;
-        if(!serverId) return;
+        if (!serverId) return;
 
         socket.join(`server_${serverId}`);
-     
+
     });
 
     socket.on("leave_server", (data) => {
         const { serverId } = data;
-        if(!serverId) return;
+        if (!serverId) return;
 
         socket.leave(`server_${serverId}`);
     });
@@ -109,33 +198,22 @@ io.on("connection", (socket) => {
     socket.on("join_channel", (data) => {
         const { channelId } = data;
         socket.join(`channel_${channelId}`);
-        console.log(key + " è entrato nel channel " + channelId);
     });
 
     socket.on("leave_channel", (data) => {
-        
+
         const { channelId } = data;
         socket.leave(`channel_${channelId}`);
-        console.log(key + " è uscito dal channel " + channelId);
+
     });
 
-    socket.on("leave_chat", (data) => {
-        const { chatId } = data;
-
-        socket.leave(`chat_${chatId}`);
-        console.log(key + " è uscito dalla chat " + chatId);
-    });
 
     socket.on("sendMessage", (data) => {
         const { id, type, message } = data;
         console.log(data);
-       
-        if (type === "channel") {
-            io.to(`channel_${id}`).emit("newMessage", message);
-        } else if (type === "chat") {
-            io.to(`chat_${id}`).emit("newMessage", message);
-        }
-       
+
+        io.to(`${type}_${id}`).emit("newMessage", message);
+
     })
 
 
@@ -144,34 +222,14 @@ io.on("connection", (socket) => {
         console.log("messaggio da eliminare", data);
 
         if (type === "channel") {
-            io.to(`channel_${id}`).emit("deletedMessage", {messageId});
-        }else if(type === "chat"){
-            io.to(`chat_${id}`).emit("deletedMessage", {messageId});
-            
+            io.to(`channel_${id}`).emit("deletedMessage", { messageId });
+        } else if (type === "chat") {
+            io.to(`chat_${id}`).emit("deletedMessage", { messageId });
+
         }
     })
 
 
-});
-
-function getKeyBySocketId(socketId: string): string | null {
-    for (const [key, socket] of map.entries()) {
-        if (socket.id === socketId) {
-            return key;
-        }
-    }
-    return null;
-}
-
-// ================= Servers =================
-let servers: Server[] = [
-    new Server(1, "Server 1", [], "Il primo server di prova"),
-    new Server(2, "Server 2", [], "Il secondo server di prova")
-];
-
-
-app.get("/servers", (req, res) => {
-    res.json(servers);
 });
 
 app.post('/token', async (req, res) => {
@@ -194,45 +252,3 @@ app.post('/token', async (req, res) => {
 httpServer.listen(4000, () => {
     console.log(`Server in ascolto sulla porta ${4000}`);
 });
-
-function emitVoiceUsersUpdate(channelId: number) {
-  const room = io.sockets.adapter.rooms.get(`channel_${channelId}`);
-  const users: string[] = [];
-
-  if (room) {
-    for (const socketId of room) {
-      const userId = getKeyBySocketId(socketId);
-      if (userId) users.push(userId);
-    }
-  }
-  const serverId = getServerIdByChannel(channelId);
-  console.log(serverId)
-  console.log("Utenti nel server partendo dal ", channelId, io.sockets.adapter.rooms.get(`server_${serverId}`));
-
-  io.to(`server_${serverId}`).emit("voice_users_update", {
-    channelId,
-    users
-  });
-  io.to(`server_${serverId}`).emit("message", {
-    channelId,
-    users
-  });
-}
-
-function getServerIdByChannel(channelId: number): number | undefined {
-  for (const server of servers) {
-    for (const section of server.sections) {
-      for (const channel of section.channels) {
-        // handle both TextChannel and VoiceChannel
-        if ("id" in channel && channel.id === channelId) {
-          console.log("Trovato serverId:", server.id, "per channelId:", channelId);
-          return server.id;
-        }
-      }
-    }
-  }
-  console.log("ChannelId non trovato:", channelId);
-  return undefined;
-}
-
-console.log("Test",getServerIdByChannel(2))
